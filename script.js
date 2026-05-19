6613affd51a675ce00be75c3ef263120ca3f9f95
Daniele Ricci <danielericci@MacBook-Pro-di-Daniele.local>
2026-05-19 22:16:14 +0200

    Aggiornamento automatico

--- script.js
+++ script.js
@@ -65,6 +65,9 @@ function cambiaFraseEBottone() {
 // 📖 Funzione per aprire un testo casuale
 function apriTestoRandom() {
   const testi = [
+    "testi/2026/Maggio/il_secchio_vuoto/il_secchio_vuoto.html",
+    "testi/2026/Maggio/La_cornice/la_cornice.html",
+    "testi/2026/Maggio/pulcinella/pulcinella.html",
     "testi/2026/marzo/Attrito_della_consapevolezza/attrito_della_consapevolezza.html",
     "testi/2025/LUGLIO/Non e tutta una questione di fede/non e tutta una questione di fede.html",
     "testi/2025/LUGLIO/non voglio essere salvato da una frase fatta/non voglio essere salvato da una frase fatta.html",
